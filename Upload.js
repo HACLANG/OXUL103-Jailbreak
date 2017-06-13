64632996d24e1d458d510c4d2d701d8edf93e6bb
iBoot <iBootBeta2@users.noreply.github.com>
2017-06-13 18:59:00 +1000

    Updates.json
    Json file updating every viewer to the current status all Developers see on their screen.

---
+++ Upload.js
@@ -0,0 +1,25 @@
+OXUL103 Jailbreak
++Updates
++Beta iPA's 
+-----------------------------------------------------------------
+**UPDATES:**
+- "App" file created. (all consistent files, and imagery of the current look of the icons, expect some glitches).
+- "Resources" embeded to require the notification to resprig and edit the running system.
+(FEWER OR SEVERAL BOOTS MAY OR WILL HAPPEN). . . The iDevice will read as the OS system isn't running as usual, and will reboot.
+Constant rebooting may have been caused by an error during the exploitation.
+- "phoneOS" now has to be recognised and diagnosed to fully get the Jailbroken files system (exploit) embeded into the device.
+- "Cydia.app" has been updated, new tweaks added for stability. Fixes ;connect to iTunes Restoration mode;.
+_ "Jailbreak running stability checked and fluently updated"/ Running file systems, only 64-bit devices capabilities.
+6:51PM:13/06/2017----------**UPDATED FREQUENTLY**-----------
+iPod Touch 6g currently unsupported, tested and caused multible bootloops.
+------------------------------------------------------------------------------------------------------------------
+**Beta iPA's**
+Beta 1: Finished and completed, needs to be refurbished, plus i'm in the need of more developers to test new beta's and testing stabilities.
+[fustrates me to buy idevices, spending a lot of money to test an exploit that may or may not work on a typical device!]
+All new developers get shoutouts in every update and commit.
+Beta 2: Almost completed, threw fewer devices into panic boot. Adding more sourcery to endure better quality and to qualify to stay open for
+multiple usage timing, and to stay active and not cause freezing, nor crashes.
+Beta 3: Yet to be developed.
+Beta 4: Yet to be developed.
+Beta 5: Yet to be developed.
+-------------------------------------------------------------------------------------------------------------------
