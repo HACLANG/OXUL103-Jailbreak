d094c9e07723d5e16ac00652d28853451c553f23
iBoot <iBootBeta2@users.noreply.github.com>
2017-06-13 22:52:28 +1000

    Create unsandboxer.h

---
+++ unsandboxer.h
@@ -0,0 +1,11 @@
+// SPECIAL THANKS TO @GeoSn0w for his "unsanbox.h" file. all credit goes to him!
+// @FCE365
+
+#ifndef unsandboxer_h
+#define unsandboxer_h
+
+#include <mach/mach.h>
+
+void start_bootstrap_unsandboxer();
+
+#endif
